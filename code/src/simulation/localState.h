4918b711554fe04bc78cda79cfe580401c8f8fd8
Adrien Husson <pangel.neu@gmail.com>
2013-11-07 23:19:00 +0100

    Interface pour la simulation

---
+++ code/src/simulation/localState.h
@@ -0,0 +1,30 @@
+#ifndef LOCAL_STATE_H
+#define LOCAL_STATE_H
+
+class Position;
+enum Layers {
+  L_Players = 1
+  L_NPC = 2
+  L_Geography = 4
+  L_Cameras = 8
+}
+
+/**
+ * @brief The Local State Class
+ * Interface to access the current game state
+ */
+
+class LocalState {
+ public :
+  /**
+   * @brief getTile
+   * @param p : position of the tile
+   * @param l : layers to return (see enum Layers)
+   * @return a copy of the tile at position p, limited to the layers specified by L
+   */
+   Tile getTile(Position p, Layers l) {
+   }
+
+} ;
+
+#endif // SIMULATION_H
