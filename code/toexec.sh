a1a4b509fe306f397a767b492c92e29b2040191f
JOURNAULT <matthieu@Edith.(none)>
2013-11-11 15:09:53 +0100

    boost added

--- code/toexec.sh
+++ code/toexec.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export x=`find -iname *.h | tr "\n" " "` ;
-export y=`find -iname *.cc | tr "\n" " "`;
+export x=`find . -iname *.h | tr "\n" " "` ;
+export y=`find . -iname *.cc | tr "\n" " "`;
 export z=`ls -R | grep : | grep src | cut -c3- | rev | cut -c2- | rev | sed '1,$ s/^/-I/' | tr "\n" " "`;
 echo "
 AUTOMAKE_OPTIONS = subdir-objects
 ACLOCAL_AMFLAGS = \${ACLOCAL_FLAGS}
 bin_PROGRAMS = game
-game_LDADD = -lsfml-graphics\$(SFML_LIB_SUFFIX) -lsfml-system\$(SFML_LIB_SUFFIX) -lsfml-window\$(SFML_LIB_SUFFIX)
+game_LDADD = -lsfml-graphics\$(SFML_LIB_SUFFIX) -lsfml-system\$(SFML_LIB_SUFFIX) -lsfml-window\$(SFML_LIB_SUFFIX) -lboost_system\$(BOOS_LIB_SUFFIX)
 game_SOURCES = $x $y
 dist_noinst_SCRIPTS = autogen.sh
 AM_CPPFLAGS = $z
