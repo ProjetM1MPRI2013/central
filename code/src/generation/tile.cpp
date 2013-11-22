88157b3815d8b2f257c818cc9909692698fca0d4
ChatanW <maxime.morgado@ens-cachan.fr>
2013-11-22 04:59:22 +0100

    class tile, création et premières fonctions

---
+++ code/src/generation/tile.cpp
@@ -0,0 +1,54 @@
+#include "tile.h"
+
+
+enum TileType {roadH, roadV, intersection, banque, immeuble}; // énumération des types de batiments
+
+Tile::Tile(int abs, int ord, TileType typeO, bool destructibleO, float anxietyO, float populationDensityO, bool gohO, bool gouO, bool gorO, bool golO, float speedO, Coordinates batOriginO) {
+  coord = new Coordinates(abs, ord);
+  type = typeO;
+  destructible = destructibleO;
+  anxiety = anxietyO;
+  populationDensity = populationDensityO;
+  goh = gohO;
+  gou = gouO;
+  gol = golO;
+  gor = gorO;
+  speed = speedO;
+  batOrigin = batOriginO;
+  lenghtBat = getTLenght(typeO);
+  weightBat = getTWeight(typeO);
+  sprite = getTSprite(typeO); //à modifier, car cela dépend si origine ou pas 
+  destructionLevel = 0.;
+}
+
+
+
+Tile::getTLenght(TileType type) {
+  switch (type) {
+  case roadH: break;
+  case roadV: break;
+  case intersection: break;
+  case banque: break;
+  case immeuble: break;
+  }
+}
+
+Tile::getTWeight(TileType type) {
+  switch (type) {
+  case roadH: break;
+  case roadV: break;
+  case intersection: break;
+  case banque: break;
+  case immeuble: break;
+  }
+}
+
+Tile::getTSprite(TileType type) {
+  switch (type) {
+  case roadH: break;
+  case roadV: break;
+  case intersection: break;
+  case banque: break;
+  case immeuble: break;
+  }
+}
---
+++ code/src/generation/tile.h
@@ -0,0 +1,171 @@
+#ifndef TILE
+#define TILE
+
+/**
+ * @brief Coordinates
+ * Useful to caracterize a tile in the map
+ */
+class Coordinates {
+ private:
+  /*
+   * abscissa 
+   */ 
+  int abs;
+  /*
+   * ordinate 
+   */
+  int ord;
+ public:
+  /**
+   * @brief create a point, with an abscissa and an ordinate
+   * @param abs : the abscissa
+   * @param ord : the ordinate
+   */
+  Coordinates(int abs, int ord);
+  /**
+   * @brief get the abscissa
+   */ 
+  int getAbs();
+  /**
+   * @brief set the abscissa
+   * @param abs : the new abscissa
+   */
+  void setAbs(int abs);
+  /**
+   * @brief get the abscissa ordinate
+   */ 
+  int getOrd();
+  /**
+   * @brief set the ordinate
+   * @param abs : the new ordinate
+   */
+  void setOrd(int ord);
+}
+;
+
+
+/**
+ * @brief A tile of the map
+ */
+class tile {
+
+ private:
+  /*
+   * type of the batiment of the case
+   */
+  TileType type;
+  Sprite sprite;
+  bool destructible;
+  float anxiety;
+  float populationDensity;
+  float destructionLevel;
+  /*
+   * possibility to go straight
+   */
+  bool goh;
+  /*
+   * possibility to go above, between 0 and 1
+   */
+  bool gou;
+  /*
+   * possibility to go to the right, between 0 and 1
+   */
+  bool gor;
+  /*
+   * possibility to go to the left, between 0 and 1
+   */
+  bool gol;
+  /*
+   * caracterisation of the speed into the tile, between 0 and 1
+   */
+  float speed;
+  /*
+   * the origin of the batiment, which is rectangular (we can decompose a batiment)
+   */
+  Coordinates batOrigin;
+  /*
+   * total lenght of the bat
+   */
+  int lenghtBat;
+  /*
+   * total weight of the bat
+   */
+  int weightBat;
+  /*
+   * coordinates of the tile
+   */
+  Coordinates coord;
+
+  /**
+   * @brief lenght of a bat thanks to its type
+   * @param type : type of the batiment
+   * @return the lenght of the bat
+   */
+  int getTLenght(TileType type);
+  /**
+   * @brief weight of a bat thanks to its type
+   * @param type : type of the batiment
+   * @return the weight of the bat
+   */
+  int getTWeight(TileType type);
+  /**
+   * @brief sprite of the bat thanks to its type
+   * @param type : type of the batiment
+   * @return the sprite of the bat
+   */
+  Sprite getTSprite(TileType type);
+  
+  public:
+  /**
+   * @brief create a tile
+   * @param abs : the abscissa of the coordinates of the tile in the map
+   * @param ord : the ordinate of the coordinates of the tile in the map
+   * @param type : type of the bat
+   * @param destructible : is this bat destructible
+   * @param anxiety : initial anxiety into the tile
+   * @param populationDensity : initial population density into the tile
+   * @param goh : initial possibility to go straight
+   * @param gou : initial possibility to go above
+   * @param gor : initial possibility to go to the right
+   * @param gol : initial possibility to go to the left
+   * @param speed : initial speed into the tile
+   * @param batOrigin : caracteristic point of the bat
+   */
+  Tile(int abs, int ord, TileType type, bool destructible, float anxiety, float populationDensity, bool goh, bool gou, bool gor, bool gol, float speed, Coordinates batOrigin);
+    TileType getType();
+    void setType(TerrainType t);
+    bool isDestructible();
+    float getAnxiety();
+    void setAnxiety(float a);
+    float getPopulationDensity();
+    void setPopulationDensity(float d);
+    float getDestructionLevel();
+    void setDestructionLevel(float dl);
+    bool getGoh();
+    void setGoh(bool goh);
+    bool getGou();
+    void setGou(bool gou);
+    bool getGor();
+    void setGor(bool gor);
+    bool getGol();
+    void setGol(bool gol);
+    float getSpeed();
+    void setSpeed(float speed);
+    Coordinates getBatOrigin();
+    void setBatOrigin(Coordinates p);
+    int getLenghtBat();
+    void setLenghtBat(int l);
+    int getWeightBat();
+    void setWeightBat(int w);
+    Sprite getSprite();
+    Coordinates getCoord();
+    void setCoord(Coordinates coord);
+    
+}
+;
+
+
+
+
+
+#endif // TILE_H
