44965bafbc2e8e86bd2d52646094ec6a4da56595
Matthieu Journault <matthieu.journault@free.fr>
2013-12-04 18:31:25 +0100

    Erreur Naina corrigé, code compile

--- code/src/graphism/tilemap.h
+++ code/src/graphism/tilemap.h
@@ -1,5 +1,17 @@
 #ifndef TILEMAP
 #define TILEMAP
+#include <TGUI/TGUI.hpp>
+#include <iostream>
+// #include <string.hpp>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <fstream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <../network/network.h>
+#include <../network/netEvent.h>
+#include <chat_event.h>
 class TileMap : public sf::Drawable, public sf::Transformable
 {
 public:
@@ -8,7 +20,10 @@ public:
 
 private:
 
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-  int* createTiles(Geography map); 
-}
-?
+  void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+  int* createTiles(Geography map);
+  sf::VertexArray m_vertices;
+  sf::Texture m_tileset;
+};
+
+#endif
