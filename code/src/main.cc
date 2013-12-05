e2a92121b8ae6cf0836c31d70f42159013a9b74c
Matthieu Journault <matthieu.journault@free.fr>
2013-12-05 15:57:43 +0100

    Quand on appuie sur quit ça quit (sweet)

--- code/src/main.cc
+++ code/src/main.cc
@@ -5,38 +5,40 @@
 #include <boost/archive/text_iarchive.hpp>
 #include <interfaceinit/interface_init.h>
 #include <generation/geography.h>
-int main()
-{
+int main() {
 	int sizeFenetre[3];
 	bool isFullScreen;
-    int b = interface_initiale(sizeFenetre,&isFullScreen);
-    std::cout << "b : " << b << std::endl;
-    //Defintion of the window
-    sf::VideoMode video_mode = sf::VideoMode(sizeFenetre[0],sizeFenetre[1],sizeFenetre[2]);
-    sf::RenderWindow window;
-    if (isFullScreen) {
-    	window.create(video_mode, "Game", sf::Style::Fullscreen);
-    }
-    else {
-    	window.create(video_mode, "Game");
-    }
-    Geography geo = Geography("424242");
+	int b = interface_initiale(sizeFenetre, &isFullScreen);
+	std::cout << "b : " << b << std::endl;
+	//Defintion of the window
+	if (b == 0) {
+		return 0;
+	} else {
+		sf::VideoMode video_mode = sf::VideoMode(sizeFenetre[0],
+				sizeFenetre[1], sizeFenetre[2]);
+		sf::RenderWindow window;
+		if (isFullScreen) {
+			window.create(video_mode, "Game", sf::Style::Fullscreen);
+		} else {
+			window.create(video_mode, "Game");
+		}
+		Geography geo = Geography("424242");
 
-	sf::Vector2u size = window.getSize();
-    unsigned int w = size.x;
-    unsigned int h = size.y;
-    //End Defintion of the window
-	while (window.isOpen()) {
-		sf::Event event;
-				while (window.pollEvent(event)) {
+		sf::Vector2u size = window.getSize();
+		unsigned int w = size.x;
+		unsigned int h = size.y;
+		//End Defintion of the window
+		while (window.isOpen()) {
+			sf::Event event;
+			while (window.pollEvent(event)) {
 
-				}
+			}
 
-				window.clear();
+			window.clear();
 
-				window.display();
+			window.display();
+		}
+		window.close();
+		return 0;
 	}
-    window.close();
-    return 0;
 }
-
