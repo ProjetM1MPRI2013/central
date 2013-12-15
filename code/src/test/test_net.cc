9b3a79609274b5c3b7d2b3002e54190b47220b34
mheinric <marc.heinrich@free.fr>
2013-12-15 21:36:50 +0100

    Test Network
    Added a more complete tests of the Network module.

--- code/src/test/test_net.cc
+++ code/src/test/test_net.cc
@@ -10,78 +10,233 @@
 
 using namespace std;
 
+class TestA : public AbstractMessage {
+
+public :
+
+  TestA(int t){
+    data = t ;
+  }
+
+  static std::string getMsgType() { return "TypeA" ;}
+
+  virtual std::string & toString() { return *(new string()) ;}
+
+  static AbstractMessage * fromString(std::string& msg){ return NULL ;}
+
+  virtual AbstractMessage* copy() {
+    return new TestA(*this) ;
+  }
+
+  int data ;
+};
+
+class TestB : public AbstractMessage {
+
+public :
+
+  TestB(int t){
+    data = t ;
+  }
+
+  static std::string getMsgType() { return "TypeB" ;}
+
+  virtual std::string & toString() { return *(new string()) ;}
+
+  static AbstractMessage * fromString(std::string& msg){ return NULL ;}
+
+  virtual AbstractMessage* copy() {
+    return new TestB(*this) ;
+  }
+
+  int data ;
+};
+
 int test_net(){
   Server *ser = Network::createDummyServer() ;
   Client *cli = Network::createDummyClient(ser) ;
-  NetEvent e ;
-  e.setType(NetEvent::PLAYER_JOIN);
-  int player_id = 0 ;
-  e.setData(&player_id);
-  cli->sendMessage(e) ;
 
-  vector<NetEvent *> v = ser->receiveMessages<NetEvent>() ;
-  if(v.size() != 1)
+  //Test from client to server.
+
+  TestA m_1(1) ;
+
+  cout << "Sending 1 message of type A from Client to Server with data  :" << endl  ;
+  cout << m_1.data << endl ;
+  cli->sendMessage<TestA>(m_1) ;
+  vector<TestA*> va= ser->receiveMessages<TestA>() ;
+  cout << "Received " << va.size() << " messages of type A with data : " << endl ;
+  for(TestA* p :va)
     {
-      cout << "wrong size : " << v.size() << endl ;
-      return 1 ;
+      cout << p->data << "; " ;
     }
-  if(v[0]->getType() != NetEvent::PLAYER_JOIN)
+  cout << endl ;
+  vector<TestB*> vb = ser->receiveMessages<TestB>() ;
+  cout << "Received " << vb.size() << " messages of type B with data : " << endl ;
+  for(TestB* p :vb)
     {
-      cout << "wrong type : " << (int) v[0]->getType() << endl ;
+      cout << p->data << "; " ;
     }
-  cout << "server : OK" << endl ;
+  cout << endl ;
 
-  ser->broadcastMessage(e) ;
-  vector<NetEvent *> v2 = cli->receiveMessages<NetEvent>() ;
-  if(v2.size() != 2)
+
+  if(va.size() == 1 && vb.size() == 0 && va[0]->data == m_1.data)
+    cout << ".....OK" << endl ;
+  else
+    cout << ".....FAIL" << endl ;
+
+
+
+  //Test from server to client.
+
+  cout << "Sending 1 message of type A from Server to Client with data  :" << endl  ;
+  cout << m_1.data << endl ;
+  ser->broadcastMessage<TestA>(m_1) ;
+  va= cli->receiveMessages<TestA>() ;
+  cout << "Received " << va.size() << " messages of type A with data : " << endl ;
+  for(TestA* p :va)
     {
-      cout << "wrong size 2: " << v2.size() << endl ;
-      return 1 ;
+      cout << p->data << "; " ;
     }
-  if(v2[1]->getType() != NetEvent::PLAYER_JOIN)
+  cout << endl ;
+  vb = cli->receiveMessages<TestB>() ;
+  cout << "Received " << vb.size() << " messages of type B with data : " << endl ;
+  for(TestB* p :vb)
     {
-      cout << "wrong type 2: " << (int) v2[0]->getType() << endl ;
-      return 1 ;
+      cout << p->data << "; " ;
     }
+  cout << endl ;
 
-  cout << "basic verification : DONE" << endl ;
-  cout << "testing with the Local and global state" << endl ;
 
-  Player locPlayer(player_id, 0,0) ;
-  Player globPlayer(player_id, 0,0) ;
-  LocalState locState(locPlayer,"");
-  Simulation globState("", std::vector<Player *>(1,&globPlayer)) ;
+  if(va.size() == 1 && vb.size() == 0 && va[0]->data == m_1.data)
+    cout << ".....OK" << endl ;
+  else
+    cout << ".....FAIL" ;
 
-  LocalStateUpdater locUpdater(&locState, cli) ;
-  UpdateGenerator globUpdater(&globState, ser) ;
+  //Test with several messages Client -> Server
 
-  int step ;
-  for(step = 0 ; step < 2 ; step ++)
+  cout << "Sending 3 messages of type A from Client to Server, with data : " << endl ;
+  int i ;
+  for(i =0 ; i < 3 ; i++)
     {
-      cout << "Global : Player moving UP" << endl ;
-
-      Player* p = globState.getPlayerByID(player_id);
-      p->setDirection(UP);
-
-      globState.run(sf::seconds(1.0f));
-
-      cout << "Global : Position of the player is now : " << p->getPosition()->getX() << ", "
-           << p->getPosition()->getY() << endl ;
-
-      cout << "Global : call update to broadcast updates" << endl ;
-
-      globUpdater.update();
-
-      cout << "Local : apply updates" << endl ;
-
-      locUpdater.update();
-
-      Player* lp = &locState.getOwnerLocalState() ;
-      cout << "Local : Observed position of the local player : " << lp->getPosition()->getX() << ", "
-           << lp->getPosition()->getY() << endl ;
-
-      cout << "Fin du step " << step << endl ;
+      TestA temp_m(i) ;
+      cli->sendMessage<TestA>(temp_m) ;
+      cout << temp_m.data << "; " ;
     }
+  cout << endl ;
+  va= ser->receiveMessages<TestA>() ;
+  cout << "Received " << va.size() << " messages of type A with data : " << endl ;
+  for(TestA* p :va)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+  vb = ser->receiveMessages<TestB>() ;
+  cout << "Received " << vb.size() << " messages of type B with data : " << endl ;
+  for(TestB* p :vb)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+
+  if(va.size() == 3 && vb.size() == 0 && va[0]->data == 0 && va[1]->data == 1 && va[2]->data ==2 )
+    cout << ".....OK" << endl ;
+  else
+    cout << ".....FAIL" << endl ;
+
+
+  //Test with several messages Server -> Client
+
+  cout << "Sending 3 messages of type A from Server to Client, with data : " << endl ;
+  for(i =0 ; i < 3 ; i++)
+    {
+      TestA temp_m(i) ;
+      ser->broadcastMessage<TestA>(temp_m) ;
+      cout << temp_m.data << "; " ;
+    }
+  cout << endl ;
+  va= cli->receiveMessages<TestA>() ;
+  cout << "Received " << va.size() << " messages of type A with data : " << endl ;
+  for(TestA* p :va)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+  vb = cli->receiveMessages<TestB>() ;
+  cout << "Received " << vb.size() << " messages of type B with data : " << endl ;
+  for(TestB* p :vb)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+
+  if(va.size() == 3 && vb.size() == 0 && va[0]->data == 0 && va[1]->data == 1 && va[2]->data ==2 )
+    cout << ".....OK" << endl ;
+  else
+    cout << ".....FAIL" << endl ;
+
+
+  //Test with several types at the same time Client -> Server
+
+  TestA me_A(10) ;
+  TestB me_B(20) ;
+  cout << "Sending messages from Client to Server :" << endl;
+  cout << "1 messages of type A with data : " <<  me_A.data << endl ;
+  cout << "1 messages of type B with data : " <<  me_B.data << endl ;
+  cli->sendMessage<TestA>(me_A) ;
+  cli->sendMessage<TestB>(me_B) ;
+
+  va= ser->receiveMessages<TestA>() ;
+  cout << "Received " << va.size() << " messages of type A with data : " << endl ;
+  for(TestA* p :va)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+  vb = ser->receiveMessages<TestB>() ;
+  cout << "Received " << vb.size() << " messages of type B with data : " << endl ;
+  for(TestB* p :vb)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+
+  if(va.size() == 1 && vb.size() == 1 && va[0]->data == me_A.data && vb[0]->data == me_B.data)
+    cout << ".....OK" << endl;
+  else
+    cout << ".....FAIL" << endl;
+
+
+  //Test with several types at the same time Server -> Client
+
+
+  cout << "Sending messages from Server to Client :" << endl;
+  cout << "1 messages of type A with data : " <<  me_A.data << endl ;
+  cout << "1 messages of type B with data : " <<  me_B.data << endl ;
+  ser->broadcastMessage<TestA>(me_A) ;
+  ser->broadcastMessage<TestB>(me_B) ;
+
+  va= cli->receiveMessages<TestA>() ;
+  cout << "Received " << va.size() << " messages of type A with data : " << endl ;
+  for(TestA* p :va)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+  vb = cli->receiveMessages<TestB>() ;
+  cout << "Received " << vb.size() << " messages of type B with data : " << endl ;
+  for(TestB* p :vb)
+    {
+      cout << p->data << "; " ;
+    }
+  cout << endl ;
+
+  if(va.size() == 1 && vb.size() == 1 && va[0]->data == me_A.data && vb[0]->data == me_B.data)
+    cout << ".....OK" << endl;
+  else
+    cout << ".....FAIL" << endl;
+
+
+
 
   return 0 ;
 }
